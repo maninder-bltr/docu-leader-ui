6c55ca2e1e0cabedfe00e9d22943f048980045b6
Maninder Singh <>
2026-03-02 23:42:09 +0530

    updated cloud url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8088/api',
+  baseURL: 'https://docu-leader-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
